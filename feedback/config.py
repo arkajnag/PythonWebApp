1d30c3433f9ccac80b702f57277332ae40b37fb4
arkajnag <arka.imps@gmail.com>
2020-05-08 10:50:43 +0200

    Commit8: Change DB server URI

--- feedback/config.py
+++ feedback/config.py
@@ -11,9 +11,9 @@ class Config:
 
 class ProductionConfig:
     SECRET_KEY = 'd9f86e26af8fa6271100809023e40c9b'
-    SQLALCHEMY_DATABASE_URI = 'postgres://jnkaoplvznlxim' \
-                              ':b6e5a9e7dfa55126166b5c0a73f066866716882c68f44e10a80c14149a72f2db@ec2-52-201-55-4' \
-                              '.compute-1.amazonaws.com:5432/deqclhoj424psa '
+    SQLALCHEMY_DATABASE_URI = 'postgres://temzjchwbahagg' \
+                              ':1bab62cebad3e2a8a5fb7d6a681c2ca3fe9b7025594b36fcdc4333677280b0a7@ec2-35-171-31-33' \
+                              '.compute-1.amazonaws.com:5432/df4pt0e86bsho '
     MAIL_SERVER = 'smtp.mailtrap.io'
     MAIL_PORT = 2525
     MAIL_USERNAME = 'ef892873b6b0c2'
