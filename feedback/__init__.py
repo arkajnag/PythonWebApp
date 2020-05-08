7ce28de6062657f1f38428013c67c37045e9f06b
arkajnag <arka.imps@gmail.com>
2020-05-08 09:24:20 +0200

    Commit4: Add Error Pages with Error Blueprint and error.app_errorhandler. Create Procfile and runtime.txt. Installed gunicorn webserver and updated requirements.txt

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-from feedback.config import Config
+from feedback.config import Config, ProductionConfig
 
 db = SQLAlchemy()
 mail = Mail()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(ProductionConfig)
     from feedback.userInput.inputRoute import inputs
+    from feedback.errors.handlers import errors
 
     db.init_app(app)
     mail.init_app(app)
     app.register_blueprint(inputs)
+    app.register_blueprint(errors)
     return app
 
 
